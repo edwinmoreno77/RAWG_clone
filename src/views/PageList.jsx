31395454b3124abe4a70a211fc65383c832a9384
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-19 23:54:49 -0300

    Fetch pages on component mount in PageList; integrate actions from context

--- src/views/PageList.jsx
+++ src/views/PageList.jsx
@@ -3,9 +3,15 @@ import { Card } from "../components/Card";
 import { Pagination } from "../components/Pagination";
 import { useContext } from "react";
 import { Context } from "../store/appContext";
+import { useEffect } from "react";
 
 export const PageList = () => {
-  const { store } = useContext(Context);
+  const { store, actions } = useContext(Context);
+
+  useEffect(() => {
+    actions.getPages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Layout>
