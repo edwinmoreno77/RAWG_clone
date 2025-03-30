55379653b913e5ac17b4e1931cfd3315d6b6dc1a
Edwin Moreno <edwinmoreno77@gmail.com>
2025-03-30 12:33:26 -0300

    Add Platforms component to display game platforms with icons; refactor Game component for better structure

---
+++ src/components/Platforms.jsx
@@ -0,0 +1,39 @@
+import { PropTypes } from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { platformIcons } from "../constants/icons";
+import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+
+export const Platforms = ({ data }) => {
+  return (
+    <div className="font-bold">
+      {data ? (
+        <div className="flex flex-wrap justify-center items-center gap-2">
+          {data?.platforms?.map((p) => {
+            const platformName = p.platform.name;
+            const icon = platformIcons[platformName] || faGamepad; // Default icon if not found
+            return (
+              <div key={platformName} className="flex items-center gap-1">
+                <FontAwesomeIcon icon={icon} className="text-white text-lg" />
+                <span className="text-white text-xs">{platformName}</span>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="animate-pulse bg-slate-500 h-4 w-48 mx-auto rounded" />
+      )}
+    </div>
+  );
+};
+
+Platforms.propTypes = {
+  data: PropTypes.shape({
+    platforms: PropTypes.arrayOf(
+      PropTypes.shape({
+        platform: PropTypes.shape({
+          name: PropTypes.string.isRequired,
+        }).isRequired,
+      })
+    ).isRequired,
+  }),
+};
--- src/views/Game.jsx
+++ src/views/Game.jsx
@@ -7,8 +7,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
 import { useTilt } from "../hooks/useTilt";
-import { platformIcons } from "../constants/icons";
-import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+import { Platforms } from "../components/Platforms";
 
 export const Game = () => {
   const { id } = useParams();
@@ -208,27 +207,7 @@ export const Game = () => {
         {/* Contenido adicional */}
         <div className="py-10 text-center bg-black">
           {/* Plataformas */}
-          <div className="font-bold">
-            {data ? (
-              <div className="flex flex-wrap justify-center items-center gap-2">
-                {data.platforms.map((p) => {
-                  const platformName = p.platform.name;
-                  const icon = platformIcons[platformName] || faGamepad; // Default icon if not found
-                  return (
-                    <div key={platformName} className="flex items-center gap-1">
-                      <FontAwesomeIcon
-                        icon={icon}
-                        className="text-white text-lg"
-                      />
-                      <span className="text-white text-xs">{platformName}</span>
-                    </div>
-                  );
-                })}
-              </div>
-            ) : (
-              <div className="animate-pulse bg-slate-500 h-4 w-48 mx-auto rounded" />
-            )}
-          </div>
+          <Platforms data={data} />
 
           {/* Géneros */}
           <div className="font-bold mt-2">
