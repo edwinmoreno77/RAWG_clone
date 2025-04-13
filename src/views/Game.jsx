438394c3663f40c6736268d4d57e771d4633c111
Edwin Moreno <edwinmoreno77@gmail.com>
2025-04-13 13:05:50 -0400

    Update video component in Game; change muted to playsInline and set volume to 30%

--- src/views/Game.jsx
+++ src/views/Game.jsx
@@ -210,11 +210,16 @@ export const Game = () => {
                   {data?.videos?.length > 0 ? (
                     <video
                       autoPlay
-                      muted
+                      playsInline
                       loop
                       controls
                       className="w-full h-auto rounded-xl shadow-lg"
                       poster={data.videos[0].preview} // Imagen de vista previa
+                      ref={(video) => {
+                        if (video) {
+                          video.volume = 0.3; // Establece el volumen al 50%
+                        }
+                      }}
                     >
                       <source src={data.videos[0].data.max} type="video/mp4" />
                       Your browser does not support the video tag.
