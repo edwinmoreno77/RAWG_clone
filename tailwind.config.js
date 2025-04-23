3b440fc66126179a19d295d3919ee2a660af99fd
Edwin Moreno <edwinmoreno77@gmail.com>
2025-04-23 19:26:03 -0400

    Add tailwind-scrollbar dependency and configure plugin in Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from "tailwind-scrollbar";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -23,5 +25,10 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    tailwindScrollbar, // Agrega el plugin aquí
+  ],
+  variants: {
+    scrollbar: ["rounded"], // Opcional: para bordes redondeados en la barra
+  },
 };
