dc9f391cc51f8cb817287949a6d3ab315a6f7648
Edwin Moreno <edwinmoreno77@gmail.com>
2025-07-31 20:22:59 -0400

    pagelist updated

--- src/pages/PageList.jsx
+++ src/pages/PageList.jsx
@@ -23,7 +23,7 @@ export const PageList = () => {
       {isLoading ? (
         <>
           <div
-            className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-${
+            className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-${
               isSidebarOpen ? "4" : "5"
             } gap-1 transition-all duration-300`}
           >
@@ -39,7 +39,7 @@ export const PageList = () => {
       ) : (
         <>
           <div
-            className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-${
+            className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-${
               isSidebarOpen ? "4" : "5"
             } gap-1 transition-all duration-300`}
           >
